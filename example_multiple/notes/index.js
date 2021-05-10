5b189238eebb79c9f687edce55608adb6e84ac93
Adrian von Gegerfelt <adrian@designbyadrian.com>
2021-05-10 15:20:57 +0200

    example: added keyboard keys to test delay

--- example_multiple/notes/index.js
+++ example_multiple/notes/index.js
@@ -1,10 +1,10 @@
 export default [
-    {name: 'c', url: '../notes/c3.mp3'},
-    {name: 'd', url: '../notes/d3.mp3'},
-    {name: 'e', url: '../notes/e3.mp3'},
-    {name: 'f', url: '../notes/f3.mp3'},
-    {name: 'g', url: '../notes/g3.mp3'},
-    {name: 'a', url: '../notes/a4.mp3'},
-    {name: 'b', url: '../notes/b4.mp3'},
-    {name: 'c', url: '../notes/c4.mp3'}
+    {name: 'c', url: '../notes/c3.mp3', key: 'a'},
+    {name: 'd', url: '../notes/d3.mp3', key: 's'},
+    {name: 'e', url: '../notes/e3.mp3', key: 'd'},
+    {name: 'f', url: '../notes/f3.mp3', key: 'f'},
+    {name: 'g', url: '../notes/g3.mp3', key: 'g'},
+    {name: 'a', url: '../notes/a4.mp3', key: 'h'},
+    {name: 'b', url: '../notes/b4.mp3', key: 'j'},
+    {name: 'c', url: '../notes/c4.mp3', key: 'k'}
 ];
